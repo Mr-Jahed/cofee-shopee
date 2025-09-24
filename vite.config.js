76031f7741420797b39b6ddd0f07c1d36129cbeb
Mr Jahed Inamdar <132382873+Mr-Jahed@users.noreply.github.com>
2025-09-24 14:47:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",   // 👈 allows external access
+    port: 5173         // 👈 matches Docker EXPOSE
+  }
 })
